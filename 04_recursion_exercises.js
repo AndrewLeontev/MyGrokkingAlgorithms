2c419d44001a0727139401ce923d88a5686b7873
Andrey Leontev <leontev.andrey.s@gmail.com>
2017-10-31 18:58:59 +1000

    replaced splice on slice

--- 04_recursion_exercises.js
+++ 04_recursion_exercises.js
@@ -4,22 +4,20 @@ const sumarr = arr => {
     if (arr.length === 0) {
     	return 0;
     }
-    return arr[arr.length - 1] + sumarr(arr.splice(0, arr.length - 1));
+    return arr[arr.length - 1] + sumarr(arr.slice(1));
 };
 
 console.log(sumarr(mas));
 
-mas = [2, 3, 4, 7, 10, 1, 4, 5, 8, 2, 3];
 // 4.2 Напишите рекурсивную функцию для подсчета элементов в списке
 const countelem = arr => {
     if (arr.length === 0) {
     	return 0;
     }
-    return 1 + countelem(arr.splice(0, arr.length - 1));
+    return 1 + countelem(arr.slice(1));
 };
 console.log(countelem(mas));
 
-mas = [2, 3, 4, 7, 10, 1, 4, 5, 8, 2, 3];
 // 4.4 найдите найбольшее число в списке
 const maxelem = arr => {
     // in progress
